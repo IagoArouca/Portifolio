832b809a785cefd805047ea629fcad0d56f10635
Iago <iagoaroucaferreira@hotmail.com>
2025-06-15 13:15:53 -0300

    Atualiza URL da API de contato para a URL de produção

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,7 @@ function Contact() {
     setStatus('Enviando...'); 
 
     try {
-      const response = await fetch('http://localhost:5000/api/contato', { 
+      const response = await fetch('https://portifoliobackend-i5s0.onrender.com', { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
